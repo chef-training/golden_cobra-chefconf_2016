f61ac2be3214eeceedfab51e0a4fee1df3602201
Franklin Webber <franklin.webber@gmail.com>
2016-07-04 22:02:28 -0500

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'golden_cobra'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures golden_cobra'
+long_description 'Installs/Configures golden_cobra'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: golden_cobra
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
